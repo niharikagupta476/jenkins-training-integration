fe4d483ca78fa68132863653f8ec8d33838d879f
Niharika Gupta <niharikag@infinite.com>
2021-05-20 23:54:33 +0530

    Added new changes

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,17 +1,17 @@
+
 def call(body) {
     def config = [:]
     config = body
     print(config)
-    def mvn_build = new src.org.utils.maven_build()
+    def mvn_build = new src.org.utils.maven
     node {
         
-
         stage('Checkout') {
             git 'https://github.com/cloudogu/jenkinsfiles'
         }
 
         stage('Build') {
-            mvn_build
+            mvn_build.maven_build()
         }
     }
 }
