aadc6305160f50005c7a153a0bd8893164ebcc58
Niharika Gupta <niharikag@infinite.com>
2021-05-20 23:47:37 +0530

    Corrected branch name

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,4 +1,16 @@
 def call(body) {
-    config = body[:]
+    def config = [:]
+    config = body
     print(config)
+    node {
+    def mvnHome = tool 'M3'
+
+    stage('Checkout') {
+        git 'https://github.com/cloudogu/jenkinsfiles'
+    }
+
+    stage('Build') {
+        sh "${mvnHome}/bin/mvn -B package"
+    }
+}
 }
